c3954913481eff2fa0d2aa0fb4c1cfb18cdbf923
WangYige1123 <3200110584@zju.edu.cn>
2021-05-10 19:50:08 +0800

    practical11

---
+++ practical11/Alignment.py
@@ -0,0 +1,6 @@
+random sequence and mouse sequence 13 5.8558558558558556%
+mouse sequence and human SOD2 protein sequence 199 89.63963963963964%
+random sequence and human SOD2 protein sequence 12 5.405405405405405%
+interpretation:
+By comparing the three, we found that human and mouse sequences were the most 
+similar, possibly because they came from the same ancestor.
