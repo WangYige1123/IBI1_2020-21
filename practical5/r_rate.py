a9a124e1fe593e9cafcfe084a0bfa323843ffdbd
WangYige1123 <3200110584@zju.edu.cn>
2021-05-12 09:13:44 +0800

    practical5

--- practical5/r_rate.py
+++ practical5/r_rate.py
@@ -2,6 +2,8 @@ n = 84
 r = 1.1
 i = 0
 while i < 5:
+# This step caculates the number of infected individuals after one round
   n = n + n * r
   i = i + 1
+# This step get the reasult which is a number rather than five numbers
 print (n)
